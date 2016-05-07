38323dc7f4749f7e089390909398652a36e36b2c
bpk20 <bpk20@zips.uakron.edu>
2016-05-07 11:16:43 -0400

    Copied amountFor() needed code and fixed the parameter

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -30,7 +30,26 @@ void Customer::addRental(const Rental& rental) {
 }
 double Customer::amountFor(const Rental& r) const
 {
-	return 0;
+	 double thisAmount = 0;
+        switch(r.getVideo().getCode()) {
+
+            case Video::REGULAR:
+            thisAmount += 2;
+            if (r.getDaysRented() > 2)
+                thisAmount += (r.getDaysRented() - 2) * 1.5;
+            break;
+
+            case Video::NEW_RELEASE:
+            thisAmount += r.getDaysRented() * 3;
+            break;
+
+            case Video::CHILDRENS:
+            thisAmount += 1.5;
+            if (r.getDaysRented() > 3)
+                thisAmount += (r.getDaysRented() - 3) * 1.5;
+            break;
+        }
+		return thisAmount;
 }
 // customer rental statement
 std::string Customer::statement() const {
